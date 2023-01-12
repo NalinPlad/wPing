469457762d1f5b55fae1153cc42a844466eb04da
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2023-01-12 08:53:24 -0800

    multithreaded

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::thread;
+use std::sync::mpsc;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+
+    // number of threads to be created
+    let num_threads = 10000;
+
+    // create send/receiver vars
+    // to move data through channel
+    let (tx, rx) = mpsc::channel();
+
+    for i in 0..num_threads {
+        let tx1 = tx.clone();
+        thread::spawn(move || {
+            tx1.send(i).unwrap();
+        });
+    }
+
+    for _ in 0..num_threads {
+        println!("{}", rx.recv().unwrap());
+    }
+    println!("Done!");
 }
