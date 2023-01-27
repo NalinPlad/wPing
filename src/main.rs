de7778d4ca6acae289732632f8c2ecf57cf86e65
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2023-01-26 21:52:30 -0800

    bump

--- src/main.rs
+++ src/main.rs
@@ -12,25 +12,24 @@ mod ip_space;
 
 fn main() {
     // number of ips to be scanned(default is all NUM_IPS, set to lower for testing)
-    let num_ips_to_scan = 1000;
+    let num_ips_to_scan = 100000;
     
-    // Create listner thread
+    // Create listner thread. Leave unblocking until all requests are sent.
     let listner_thread = thread::spawn(move || {
         listen();
     });
 
-    // listner_thread.join().unwrap();
-
     // create send/receiver vars
-    // to move data through channel
-    let (tx, rx) = channel();
+    let (tx, _rx) = channel();
 
     // Start timer
     let start = Instant::now();
 
+    // Initialize algorithm variables
     let mut step:u32 = 0;
     let mut visited = vec![false; NUM_IPS.try_into().unwrap()];
     
+    // Send ICMP requests
     for _ in 0..num_ips_to_scan {
         let tx1 = tx.clone();
         
@@ -42,12 +41,14 @@ fn main() {
         tx1.send(target.to_string()).unwrap();
     }
 
-    // End Timer
+    // End Timer for sending
     let duration = start.elapsed();
     
+    // Print debug
     println!("Done sending! took {:?} for {:?} targets, or {:?} hours for ipv4", duration, num_ips_to_scan, (((duration / num_ips_to_scan) * 4_294_967_295) / 60) / 60);
     
-
+    // Start blocking main thread for listner
+    listner_thread.join().unwrap();
 
     // for _ in 0..num_ips_to_scan {
     //     println!("{}", rx.recv().unwrap());
