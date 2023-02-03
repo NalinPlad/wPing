978237087967639eb452c1db2f7d69449f08dc6e
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2023-02-03 13:25:06 -0800

    bump

--- src/icmp/save_data.rs
+++ src/icmp/save_data.rs
@@ -2,54 +2,47 @@ use std::{fs::OpenOptions, io::Write};
 use csv::Reader;
 
 pub fn init_file(file_name: &str) {
-    let mut file = OpenOptions::new()
+    let mut _file = OpenOptions::new()
         .write(true)
         .create(true)
         .truncate(true)
         .open(file_name)
         .unwrap();
 
-    let header = "block,count\n";
-    file.write_all(header.as_bytes()).unwrap();
+    // let header = "block,count\n";
+    // file.write_all(header.as_bytes()).unwrap();
+}
+
+fn block_to_number(ip_block: &str) -> u32 {
+    let parts: Vec<&str> = ip_block.split("/").collect();
+    let ip = parts[0];
+    let mask = parts[1].parse::<u32>().unwrap();
+
+    let ip_parts: Vec<&str> = ip.split(".").collect();
+    let ip_nums: [u32; 4] = [
+        ip_parts[0].parse::<u32>().unwrap(),
+        ip_parts[1].parse::<u32>().unwrap(),
+        ip_parts[2].parse::<u32>().unwrap(),
+        ip_parts[3].parse::<u32>().unwrap(),
+    ];
+
+    let mut number: u32 = 0;
+    for i in 0..4 {
+        number += ip_nums[i] * (256u32.pow(3 - i as u32));
+    }
+
+    number >> (32 - mask)
 }
 
 pub fn write_data(file_name: String, block: String) {
-    let mut rdr = Reader::from_path(&file_name).unwrap();
-    let mut found = false;
-
-    for result in rdr.records() {
-        let record = result.unwrap();
-        let current_block = record.get(0).unwrap();
-        let current_count: u32 = record.get(1).unwrap().parse().unwrap();
-
-        if current_block == block {
-            found = true;
-            let new_count = current_count + 1;
-
-            // Open the file in write mode
-            let mut file = OpenOptions::new()
-                .write(true)
-                .append(true)
-                .open(&file_name)
-                .unwrap();
-
-            // Replace the current name and score with the updated values
-            let data = format!("{},{}\n", block, new_count);
-            let bytes = data.as_bytes();
-            file.write_all(bytes).unwrap();
-            break;
-        }
-    }
-
-    if !found {
-        let mut file = OpenOptions::new()
-            .write(true)
-            .append(true)
-            .open(&file_name)
-            .unwrap();
-
-        let data = format!("{},1\n", block);
-        let bytes = data.as_bytes();
-        file.write_all(bytes).unwrap();
-    }
+    // Open the file in write mode
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(&file_name)
+        .unwrap();
+    
+    // Write block
+    let b_data = block_to_number(&block).to_string() + "\n"; 
+    file.write_all(b_data.as_bytes()).unwrap();
 }
